216858e22ceeec6ae5e0eeedb14335cf526a7b2f
Mayank Solanki <mayank.solanki@live.com>
2017-07-19 15:32:46 +0100

    making test more parametirised

--- src/main/PerfectNumber.java
+++ src/main/PerfectNumber.java
@@ -1,34 +1,45 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PerfectNumber {
+
+    public static int startingNumber, endingNumber;
+    public static ArrayList<Integer> perfectNumbers;
 
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner (System.in);
 
         System.out.println("Enter lower boundary number: ");
-        int startingNumber = keyboard.nextInt();
+        startingNumber = keyboard.nextInt();
 
         System.out.println("Enter upper boundary number: ");
-        int endingNumber = keyboard.nextInt();
+        endingNumber = keyboard.nextInt();
 
         System.out.println("Looking for Perfect Numbers between " +startingNumber + " and " +endingNumber);
+        perfectNumbers = validatePerfectNumber(startingNumber, endingNumber);
+    }
+
+    public static ArrayList<Integer> validatePerfectNumber(int startingNumber, int endingNumber) {
 
         if(startingNumber % 2 != 0) {
             startingNumber = startingNumber + 1;
         }
 
+        ArrayList<Integer> perfectno = new ArrayList<>();
         for (int i = startingNumber; i <= endingNumber; i+=2) {
             if (i % 10 == 6 || i % 10 == 8) {
                 if (isPerfectNumber(i)) {
+                    perfectno.add(i);
                     System.out.println(i + " is a perfect number");
                 }
             }
         }
-
+        return perfectno;
     }
+
     public static boolean isPerfectNumber(int number) {
         int sum=0;
         for(int i=1; i<=number/2; i++) {
--- src/test/PerfectNumberTest.java
+++ src/test/PerfectNumberTest.java
@@ -30,24 +30,12 @@ public class PerfectNumberTest {
 
     @Test
     public void findPerfectNumberInGivenRangeTest() {
-        int startingNumber = 33550300;
-        int endingNumber = 33550400;
+        PerfectNumber.startingNumber = 33550300;
+        PerfectNumber.endingNumber = 33550400;
         ArrayList<Integer> expected = new ArrayList<>();
         expected.add(33550336);
 
-        ArrayList<Integer> actual = new ArrayList<>();
-
-        if(startingNumber % 2 != 0) {
-            startingNumber = startingNumber + 1;
-        }
-
-        for (int i = startingNumber; i <= endingNumber; i+=2) {
-            if (i % 10 == 6 || i % 10 == 8) {
-                if (PerfectNumber.isPerfectNumber(i)) {
-                    actual.add(i);
-                }
-            }
-        }
+        ArrayList<Integer> actual = PerfectNumber.validatePerfectNumber(PerfectNumber.startingNumber, PerfectNumber.endingNumber);
         Assert.assertEquals(actual,expected);
     }
 
